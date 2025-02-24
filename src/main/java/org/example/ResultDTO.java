1be28c0ad7198ae2b814baf89164695e98563f5d
navpreet-git <navpreet.kaur.0206@gmail.com>
2025-02-24 15:41:04 +0100

    utility methods

--- src/main/java/org/example/ResultDTO.java
+++ src/main/java/org/example/ResultDTO.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ResultDTO {
@@ -7,8 +8,41 @@ public class ResultDTO {
     private List<ResultDTO> dipendoDa;
     private List<ResultDTO> servoPer;
 
-    public ResultDTO(String code){
-        this.codiceProgressivo = code;
+    public ResultDTO(String codiceProgressivo) {
+        this.codiceProgressivo = codiceProgressivo;
+        this.dipendoDa = new ArrayList<>();
+        this.servoPer = new ArrayList<>();
     }
 
+    public List<ResultDTO> getDipendoDa() {
+        return dipendoDa;
+    }
+
+    public List<ResultDTO> getServoPer() {
+        return servoPer;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(codiceProgressivo).append("\n");
+        result.append("dipende da: \n");
+        if (!dipendoDa.isEmpty()) {
+            for (ResultDTO dto : dipendoDa) {
+                result.append("-").append(dto.codiceProgressivo).append(" ");
+            }
+        } else {
+            result.append("None");
+        }
+        result.append("\n");
+        result.append("serve per:\n ");
+        if (!servoPer.isEmpty()) {
+            for (ResultDTO dto : servoPer) {
+                result.append(dto.codiceProgressivo).append(" ");
+            }
+        } else {
+            result.append("None");
+        }
+        return result.toString();
+    }
 }
